68a49dac391e74b88b5fba5866cf029ab2e1a4d6
SmallHanley <smallhanley@gmail.com>
2022-03-04 20:18:04 +0800

    Fix invalid read of size bug

--- queue.c
+++ queue.c
@@ -119,7 +119,7 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
     element_t *elm = list_first_entry(head, element_t, list);
     list_del_init(&elm->list);
     if (sp) {
-        memcpy(sp, elm->value, bufsize);
+        strncpy(sp, elm->value, bufsize - 1);
         sp[bufsize - 1] = '\0';
     }
     return elm;
@@ -137,7 +137,7 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
     element_t *elm = list_last_entry(head, element_t, list);
     list_del_init(&elm->list);
     if (sp) {
-        memcpy(sp, elm->value, bufsize);
+        strncpy(sp, elm->value, bufsize);
         sp[bufsize - 1] = '\0';
     }
     return elm;
